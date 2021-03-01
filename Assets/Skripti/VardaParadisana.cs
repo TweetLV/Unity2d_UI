9d5e23dc5b78928af7615e40ca9d12b127be5662
TweetLV <gas3210@inbox.lv>
2021-03-01 16:06:18 +0200

    1. lekcijas uzdevums pabeigts

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Jaieimporte, lai varetu stradat ar UI elementiem
+using UnityEngine.UI;
+
+
+public class VardaParadisana : MonoBehaviour{
+    //Mainīgajā saglabās tekstu ko raksta teksta laukā
+    public string teksts;
+    //Lauks, kurā ieraksta tekstu
+    public GameObject ievadesLauks;
+    //Kur attelot tekstu
+    public GameObject tekstaAttelosana;
+
+    //Funkcija tiek izsaukta nospiežot pogu
+    public void uzglabatTekstu(){
+        teksts = ievadesLauks.GetComponent<Text>().text;
+        tekstaAttelosana.GetComponent<Text>().text = "Sveiks " + teksts.ToUpper() + "!";
+            ;
+    }
+
+}
