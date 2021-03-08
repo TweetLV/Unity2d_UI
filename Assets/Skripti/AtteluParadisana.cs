a865fdba2698be14d3af0f4db54ec312afd2d2c0
TweetLV <gas3210@inbox.lv>
2021-03-08 15:07:32 +0200

    Iespēja paslēpt/parādīt bildes

---
+++ Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtteluParadisana : MonoBehaviour{
+    //Uzglabās mūsu bildes no ainas
+    public GameObject bins;
+    public GameObject lacitis;
+    public GameObject tante;
+    public GameObject masina;
+
+    public void binaAttelosana(bool vertiba)
+    {
+        bins.SetActive(vertiba);
+    }
+    public void lacitisAttelosana(bool vertiba)
+    {
+        lacitis.SetActive(vertiba);
+    }
+    public void tanteAttelosana(bool vertiba)
+    {
+        tante.SetActive(vertiba);
+    }
+    public void masinaAttelosana(bool vertiba)
+    {
+        masina.SetActive(vertiba);
+    }
+    public void paKreisiBins(bool vertiba)
+    {
+        bins.transform.localScale = new Vector2(1,1);
+    }
+    public void paLabiBins(bool vertiba)
+    {
+        bins.transform.localScale = new Vector2(-1, 1);
+    }
+}
--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Pirmais Unity 2D projekts, pagūsim UI elementus.
 ### Veicamo darbu saraksts
 - [x] Nodrošināt projekta versionēšanu
 - [x] Ievietot teksta logu, teksta ievades lauku un pogu
-- [ ] Ievietot attēlus
-- [ ] ievietot ček boxus
-- [ ] Nodrošināt radio pogas darbību
+- [x] Ievietot attēlus
+- [x] ievietot ček boxus
+- [x] Nodrošināt radio pogas darbību
 - [ ] Izveidot izkrītošā saraksta pogu
 - [ ] Nodrošināt attēla izmēra maiņu ar bīdjoslu
 - [ ] Nodrošināt attēla pārvietošanu ar peles palīdzību
